92035b68288e925b88b0abebe7184c67efbb95a3
Ridwan Kadri <kadriridwan0@gmail.com>
2021-02-26 00:10:17 +0000

    application folder structure and dependencies
    added needed dependencies and added relevant folders for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Netflix</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/browse'>
+          <p>I will be the sign in page</p>
+        </Route>
+        <Route path='/signin'>
+          <p>I will be the sign up page</p>
+        </Route>
+        <Route path='/browse'>
+          <p>I will be the browse page</p>
+        </Route>
+        <Route path='/'>
+          <p>I am going to be a cloned netflix app</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNKI4FQsuA6pdysIKmSjgRKk7mRdGZyd0",
+    authDomain: "netflix-clone-a60fc.firebaseapp.com",
+    projectId: "netflix-clone-a60fc",
+    storageBucket: "netflix-clone-a60fc.appspot.com",
+    messagingSenderId: "926819612748",
+    appId: "1:926819612748:web:66356283acb84b45c71ce9"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
