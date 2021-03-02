5c2f75e73e8ed7a26f49f473bfbab7bf77ad7e73
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-02 18:13:57 +0000

    styling signin page and signin form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,13 +3,45 @@ import { Form } from '../components'
 import {HeaderContainer} from '../containers/header'
 
 export default function SignIn() {
-    const [error, setError] = useState('')
+    const [error, setError] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <HeaderContainer>
             <Form>
                 <Form.Title>Sign In</Form.Title>
                 {error && <Form.Error>{error}</Form.Error>}
+
+                <Form.Base onSubmit={handleSignIn} method='POST'>
+                    <Form.Input
+                        placeholder="Email Address"
+                        value={emailAddress}
+                        onChange={({target}) => setEmailAddress(target.value)}
+                    />
+                    <Form.Input
+                        type="password"
+                        value={password}
+                        autoComplete = "off"
+                        placeholder="Password"
+                        onChange={({target}) => setPassword(target.value)}
+                    />
+                    <Form.Submit disabled={false} type="submit">
+                        Sign In
+                    </Form.Submit>
+
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA.
+                    </Form.TextSmall>
+                </Form.Base>
             </Form>
         </HeaderContainer>
     )
