c73fbf3f78f4a4d19225b65faa00f03b1649c2a7
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-02 13:55:49 +0000

    hooked and connected to firebase

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {GlobalStyles} from './global-styles'
 import App from './App';
+import {FirebaseContext} from './context/firebase';
 
+
+const config = {
+    apiKey: "AIzaSyDNKI4FQsuA6pdysIKmSjgRKk7mRdGZyd0",
+    authDomain: "netflix-clone-a60fc.firebaseapp.com",
+    projectId: "netflix-clone-a60fc",
+    storageBucket: "netflix-clone-a60fc.appspot.com",
+    messagingSenderId: "926819612748",
+    appId: "1:926819612748:web:66356283acb84b45c71ce9"
+}
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles/>
-    <App />
-  </React.StrictMode>,
+    <FirebaseContext.Provider value={{firebase: window.firebase}}>
+      <GlobalStyles/>
+      <App />
+    </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
