b3b61ee2fc9cdcfdd0fbd002ea610b23ba278073
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-03 00:05:43 +0000

    added firebase auth to sign in and sign up page by username and password

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,17 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {useHistory} from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
 import { Form } from '../components'
 import {HeaderContainer} from '../containers/header'
 import {FooterContainer} from '../containers/footer'
+import * as ROUTES from '../constants/routes'
 
 export default function SignIn() {
-    const [error, setError] = useState('');
+    const history = useHistory();
+    const {firebase} = useContext(FirebaseContext);
+
+    
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('');
 
     const isInvalid = password === '' | emailAddress === '';
 
     const handleSignIn = (e) => {
         e.preventDefault();
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(()=> {
+                setEmailAddress('');
+                setPassword('');
+                setError('');
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error)=> setError(error.message))
     }
 
     return (
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,16 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {useHistory} from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
 import { Form } from '../components'
 import {HeaderContainer} from '../containers/header'
 import {FooterContainer} from '../containers/footer'
 import * as ROUTES from '../constants/routes'
 
+
 export default function SignUp() {
+    const history = useHistory();
+    const {firebase} = useContext(FirebaseContext);
+
     const [firstName, setFirstName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('')
@@ -14,8 +20,24 @@ export default function SignUp() {
 
     const handleSignUp = (e) => {
         e.preventDefault();
-    }
 
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then((result) => 
+                result.user
+                .updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.floor(Math.random() * 5) + 1,
+                })
+                .then(()=> {
+                    setEmailAddress('');
+                    setPassword('');
+                    setError('');
+                    history.push(ROUTES.HOME)
+                })
+            ).catch((error)=> setError(error.Message))
+    };
 
     return(
         <>
