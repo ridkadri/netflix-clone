865284db079af3c51e57a1da5d200a7586ea2e52
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-04 15:26:38 +0000

    added feature and searchbox components to the header in the browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -9,6 +9,7 @@ export function BrowseContainer() {
     const [profile, setProfile] = useState({});
     const [category, setCategory] = useState('series');
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const {Firebase} = useContext(FirebaseContext);
 
@@ -33,6 +34,9 @@ export function BrowseContainer() {
                             onClick={()=> setCategory('films')}>
                             Films
                         </Header.Link>
+                    </Header.Group>
+                    <Header.Group>
+                        <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                     </Header.Group>
                 </Header.Frame>
 
